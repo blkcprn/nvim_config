882dd5afc989bef7c9da32e1b5997153244995ac
blkcprn <blkcprn@gmail.com>
2025-01-06 09:02:19 +0200

    Update toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,13 +1,13 @@
 return {
-    'akinsho/toggleterm.nvim',
+    "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
         require("toggleterm").setup({
             open_mapping = [[<C-\>]],
             direction = "horizontal"
         })
-        vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>')
-        vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical<CR>')
-        vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<CR>')
+        vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
+        vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>")
+        vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
     end
 }
